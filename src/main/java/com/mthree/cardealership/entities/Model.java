edd8376cd7d43fb3328a2a62561114fe7da2f48d
ychen8777 <ychen8777@gmail.com>
2021-09-27 15:08:59 -0400

    create model classes Make, Model, Special

---
+++ src/main/java/com/mthree/cardealership/entities/Model.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mthree.cardealership.entities;
+
+import java.util.Objects;
+import org.springframework.stereotype.Component;
+
+/**
+ * Model of car
+ * @author ychen
+ */
+@Component
+public class Model {
+    
+    private int id;
+    private String model;
+    private int makeID;
+    private int userID;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getMakeID() {
+        return makeID;
+    }
+
+    public void setMakeID(int makeID) {
+        this.makeID = makeID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 47 * hash + this.id;
+        hash = 47 * hash + Objects.hashCode(this.model);
+        hash = 47 * hash + this.makeID;
+        hash = 47 * hash + this.userID;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Model other = (Model) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (this.makeID != other.makeID) {
+            return false;
+        }
+        if (this.userID != other.userID) {
+            return false;
+        }
+        if (!Objects.equals(this.model, other.model)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+}
