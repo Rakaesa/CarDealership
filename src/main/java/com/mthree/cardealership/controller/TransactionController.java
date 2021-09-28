0843b15b9e269faff393d3678adfc256493bd1cf
anlimin <anlimin1978@hotmail.com>
2021-09-28 11:06:13 -0400

    updated transaction controller

--- src/main/java/com/mthree/cardealership/controller/TransactionController.java
+++ src/main/java/com/mthree/cardealership/controller/TransactionController.java
@@ -5,10 +5,52 @@
  */
 package com.mthree.cardealership.controller;
 
+import com.mthree.cardealership.dao.TransactionDao;
+import com.mthree.cardealership.entities.Contact;
+import com.mthree.cardealership.entities.Transaction;
+import com.mthree.cardealership.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
 /**
  *
  * @author conno
  */
+@Controller
 public class TransactionController {
+    @Autowired
+    TransactionDao transactionDao;
+
+    @GetMapping("transactions")
+    public String displayTransactions(Model model) {
+        List<Transaction> transactions = transactionDao.getAllTransactions();
+        model.addAttribute("transactions", transactions);
+        return "admin/transactions";
+    }
+
+    @GetMapping("transactionsForUser")
+    public String displayTransactionsForUser(User user, Model model) {
+        List<Transaction> transactions = transactionDao.getTransactionsForUser(user);
+        model.addAttribute("transactions", transactions);
+        return "admin/transactions";
+    }
+
+    @PostMapping("addTransaction")
+    public String addTransaction(Transaction transaction, HttpServletRequest request) {
+        transactionDao.addTransaction(transaction);
+        return "redirect:/admin/transactions";
+    }
+
+    @GetMapping("deleteTransaction")
+    public String deleteTransaction(Integer id) {
+        transactionDao.deleteTransactionById(id);
+        return "redirect:/admin/transactions";
+    }
     
 }
