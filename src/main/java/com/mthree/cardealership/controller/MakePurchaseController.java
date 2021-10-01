2eb16b677748d4204e28aabfd63e9f4f31e62181
ychen8777 <ychen8777@gmail.com>
2021-09-30 22:09:10 -0400

    add PostMapping, edit form display

--- src/main/java/com/mthree/cardealership/controller/MakePurchaseController.java
+++ src/main/java/com/mthree/cardealership/controller/MakePurchaseController.java
@@ -8,11 +8,16 @@ package com.mthree.cardealership.controller;
 import com.mthree.cardealership.dao.CarDao;
 import com.mthree.cardealership.dao.TransactionDao;
 import com.mthree.cardealership.entities.Car;
+import com.mthree.cardealership.entities.CarModel;
+import com.mthree.cardealership.service.CarModelService;
+import com.mthree.cardealership.service.MakeService;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  *
@@ -25,15 +30,39 @@ public class MakePurchaseController {
     CarDao carDao;
     
     @Autowired
+    MakeService makeService;
+    
+    @Autowired
+    CarModelService carModelService;
+    
+    @Autowired
     TransactionDao transactionDao;
     
     @GetMapping("sales/makepurchase/{id}")
     public String getMakePurchasePage(@PathVariable(value="id") int id, Model model) {
         
         Car curCar = carDao.getCarById(id);
+        CarModel curModel = carModelService.getCarModelById(curCar.getModelID());
+        String makeName = makeService.getMakeNameById(curModel.getMakeID());
+        String modelName = curModel.getModel();
+        
         model.addAttribute("car", curCar);
+        model.addAttribute("makeName", makeName);
+        model.addAttribute("modelName", modelName);
+        
+        System.out.println("isPurchased: " + curCar.isIsPurchased());
         
         return "makePurchase";
+    }
+    
+    @PostMapping("sales/makepurchase/addTransaction")
+    public String addTransaction(HttpServletRequest request) {
+        int carId = Integer.parseInt(request.getParameter("carId"));
+        Car curCar = carDao.getCarById(carId);
+        
+        //carDao.markCarPurchased(curCar);
+        
+        return "redirect:/sales/makepurchase/" + carId;
     }
     
     
